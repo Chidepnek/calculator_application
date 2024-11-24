2f6d8a4553abb14c64dc1779a94d12f3394a20eb
Chi Le <180340526+Chidepnek@users.noreply.github.com>
2024-11-24 21:43:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from subtraction import subtraction
 from add import addition
 from multiplication import multiplication
 from division import division
-from Exponent import exponent
+from exponent import exponent
